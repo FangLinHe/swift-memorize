bd5d98bc747b61c185cd596a5cea97d938741303
Fang-Lin He <fanglin.he.ms@gmail.com>
2020-12-24 16:57:33 +0100

    Lecture 1: show cards with emojis on it

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by FANG LIN HE on 2020/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            ForEach(0..<6) { index in
+                CardRowView(rowNumber: index)
+            }
+        }
+        .foregroundColor(.orange)
+        .padding()
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            }
+        } else {
+            RoundedRectangle(cornerRadius: 10.0)
+        }
+    }
+}
+
+struct CardRowView: View {
+    var rowNumber: Int
+    var body: some View {
+        HStack(spacing: 10) {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: (rowNumber + index) % 2 == 0)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by FANG LIN HE on 2020/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
